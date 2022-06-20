c2da77ad701b64d660dcb39fb33dff04fa5b5b44
Ashish <aashish.bhujel@gmail.com>
2022-06-20 16:17:58 +0545

    Added : Header for app
    Might just get rid as development conti ..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
  
 import './App.css';
+import Header from './components/Header';
 import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <login/>
+   
         <Login/>
     </div>
   );
